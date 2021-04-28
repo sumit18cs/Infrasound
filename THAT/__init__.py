c824b544647640c70a35822af890179dca5384ce
Sumit Kumar Yadav <47476553+sumit18cs@users.noreply.github.com>
2021-04-28 11:10:36 +0530

    Update __init__.py

--- THAT/__init__.py
+++ THAT/__init__.py
@@ -6,22 +6,22 @@ from flask_mail import Mail
 from kombu.utils.url import safequote
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 ALLOWED_EXTENSIONS = {'mp4'}
 
-application.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-application.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 #application.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
 
-db=SQLAlchemy(application)
-bcrypt =Bcrypt(application)
-login_manager=LoginManager(application)
+db=SQLAlchemy(app)
+bcrypt =Bcrypt(app)
+login_manager=LoginManager(app)
 login_manager.login_view='login' #setting the route to login
 login_manager.login_message_category='info'
-application.config['MAIL_SERVER']='smtp.googlemail.com'
-application.config['MAIL_USE_TLS']=587
-application.config['MAIL_USERNAME']='sumit.11.sky@gmail.com'
-application.config['MAIL_PASSWORD']='password'
-mail=Mail(application)
+app.config['MAIL_SERVER']='smtp.googlemail.com'
+app.config['MAIL_USE_TLS']=587
+app.config['MAIL_USERNAME']='sumit.11.sky@gmail.com'
+app.config['MAIL_PASSWORD']='password'
+mail=Mail(app)
 from THAT import routes
